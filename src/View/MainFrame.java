40ffadec0a7d93ade78c52c12608f0ec7547f6a2
marko <mnikacevic1620rn@raf.rs>
2021-10-24 03:36:31 +0200

    napravio split

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -18,22 +18,41 @@ public class MainFrame extends JFrame {
 
     private void init() {
         actionManager = new ActionManager();
+    }
+
+    private void initGUI() {
         setLayout(new BorderLayout());
         setTitle("RuDok");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize(screenSize.width / 2, screenSize.height / 2);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 
         menu = new Menu();
         setJMenuBar(menu);
         add(new Toolbar(), BorderLayout.NORTH);
+
+        JPanel desktop = new JPanel();
+        JScrollPane scroll = new JScrollPane();
+        scroll.setMinimumSize(new Dimension(200, 150));
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, desktop);
+        getContentPane().add(split, BorderLayout.CENTER);
+        split.setDividerLocation(250);
+        split.setOneTouchExpandable(true);
+
+
+
+
+
+
+
+        setVisible(true);
     }
 
     public static MainFrame getInstance() {
         if (instance == null) {
             instance = new MainFrame();
             instance.init();
+            instance.initGUI();
         }
         return instance;
     }

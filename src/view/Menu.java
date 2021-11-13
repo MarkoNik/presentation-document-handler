8412c1c001d037cd3aaff45003cf078fa12d2165
marko <mnikacevic1620rn@raf.rs>
2021-11-13 14:41:52 +0100

    dodao dugme edit i premestio akcije za menjaje autora i teme tamo

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -7,14 +7,15 @@ public class Menu extends JMenuBar {
     public Menu() {
         JMenu menuFile = new JMenu("File");
         menuFile.add(MainFrame.getInstance().getActionManager().getNewAction());
-
-        JMenu menuFilePresentation = new JMenu("Presentation");
-
-        menuFilePresentation.add(MainFrame.getInstance().getActionManager().getChangeAuthorAction());
-        menuFilePresentation.add(MainFrame.getInstance().getActionManager().getChangeThemeAction());
-
-        menuFile.add(menuFilePresentation);
+        menuFile.add(MainFrame.getInstance().getActionManager().getDeleteAction());
         add(menuFile);
+
+
+        JMenu menuEdit = new JMenu("Edit");
+        menuEdit.add(MainFrame.getInstance().getActionManager().getChangeAuthorAction());
+        menuEdit.add(MainFrame.getInstance().getActionManager().getChangeThemeAction());
+        add(menuEdit);
+
 
         JMenu menuHelp = new JMenu("Help");
         menuHelp.add(MainFrame.getInstance().getActionManager().getInfoAction());
